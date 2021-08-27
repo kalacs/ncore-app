5fceef3211e87e68974c3e467354409deef7acc7
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2021-08-27 22:46:58 +0200

    Modify the structure of the project to make more sense

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const { app, BrowserWindow, Notification } = require('electron');
-const path = require('path');
-const makeDlnaCast = require('./services/dlna.js');
-const makeTorrentClient = require("./services/torrent_client.js");
+const { app, BrowserWindow, Notification } = require("electron");
+const path = require("path");
+const makeDlnaCast = require("./src/services/dlna.js");
+const makeTorrentClient = require("./src/services/torrent_client.js");
 const torrentClientConfig = {};
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
@@ -14,21 +15,30 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, "./renderer/preload.js"),
+    },
   });
 
-  mainWindow.loadURL('https://ncore.pro');
+  mainWindow.loadURL("https://ncore.pro");
   mainWindow.webContents.openDevTools();
 };
 
 // 1. Start searching for dlna devices
 const dlnacast = makeDlnaCast();
-dlnacast.startSearch().then((media) => {
-  new Notification({ title: 'Search for dlna devices', body: `A device has been found ${media.name}`}).show()
-}).catch((error) => {
-  new Notification({ title: 'Search for dlna devices', body: 'No device has been found!'}).show()
-});
+dlnacast
+  .startSearch()
+  .then((media) => {
+    new Notification({
+      title: "Search for dlna devices",
+      body: `A device has been found ${media.name}`,
+    }).show();
+  })
+  .catch((error) => {
+    new Notification({
+      title: "Search for dlna devices",
+      body: "No device has been found!",
+    }).show();
+  });
 
 // 2. Start torrent client
 const client = makeTorrentClient(torrentClientConfig);
@@ -36,18 +46,18 @@ const client = makeTorrentClient(torrentClientConfig);
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
