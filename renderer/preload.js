f94be590c9b8da8cb152269a42ac927cf48286fd
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2021-08-30 00:12:12 +0200

    Add play button to frontend side

--- renderer/preload.js
+++ renderer/preload.js
@@ -1,5 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const torrents = [...document.querySelectorAll('.torrent_txt > a').values()].map(({ title }) => title);
-  console.log(torrents);
-})
+const renderPlayButton = (parentElement) => {
+  const playButtonElement = document.createElement("a");
+  playButtonElement.className = "play-button";
+  playButtonElement.title = "Lejátszás TV-én (ÚJ)";
+  playButtonElement.innerText = `▶`;
+  playButtonElement.onclick = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    // download torrent
+  };
 
+  parentElement.append(playButtonElement);
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll(".tabla_szoveg a > nobr")
+    .forEach((element) => renderPlayButton(element));
+});
