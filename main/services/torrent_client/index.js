dc5ce37adb5b85b01d4c68685182f536e41f3b9a
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2021-09-01 00:18:21 +0200

    Feature/start leeching torrent (#4)
    * Change package version
    
    * Start downloading torrent and send statistics to the frontend

--- main/services/torrent_client/index.js
+++ main/services/torrent_client/index.js
@@ -1,15 +1,38 @@
+const { app, webContents } = require("electron");
 const makeTorrentClient = require("./lib");
 
 module.exports = function (config) {
   let torrentClient;
+  let clientStatInterval;
 
   return {
     start() {
       torrentClient = makeTorrentClient(config);
+
+      app.whenReady().then(() => {
+        const [currentWebContents] = webContents.getAllWebContents();
+        clientStatInterval = setInterval(() => {
+          currentWebContents.send(
+            "torrent-client-service/state",
+            torrentClient.getClientStat()
+          );
+        }, 1000);
+      });
     },
-    stop() {},
+    stop() {
+      clearInterval(clientStatInterval);
+    },
     getTorrentFileFolder() {
       return torrentClient.getTorrentFileFolder();
+    },
+    pauseAllSeedableTorrent() {
+      return torrentClient.pauseAllSeedableTorrent();
+    },
+    resumeAllSeedableTorrent() {
+      return torrentClient.resumeAllSeedableTorrent();
+    },
+    addTorrent(fileName) {
+      return torrentClient.addTorrent(fileName);
     },
   };
 };
