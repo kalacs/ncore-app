97730c9cd9eec3fffa0b1404578e94be8c9bdfa2
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2021-09-06 22:33:58 +0200

    Start streaming from the new frontend (#6)

--- renderer/preload.js
+++ renderer/preload.js
@@ -11,10 +11,13 @@ const extractTorrentFileId = (element) => {
 };
 
 const renderPlayButton = (parentElement) => {
-  const playButtonElement = document.createElement("a");
+  const playButtonElement = document.createElement("span");
   playButtonElement.className = "play-button";
   playButtonElement.title = "Lejátszás TV-én (ÚJ)";
   playButtonElement.innerText = `▶`;
+  playButtonElement.style.position = "absolute";
+  playButtonElement.style.left = "60px";
+  playButtonElement.style.cursor = "pointer";
   playButtonElement.onclick = function (event) {
     event.preventDefault();
     event.stopPropagation();
@@ -27,7 +30,30 @@ const renderPlayButton = (parentElement) => {
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelectorAll(".tabla_szoveg a > nobr")
-    .forEach((element) => renderPlayButton(element));
+  setTimeout(() => {
+    const torrentList = document.getElementsByTagName("tor-browse-list");
+    if (torrentList.length) {
+      document
+        .querySelectorAll("tor-torrent-release a.mat-caption")
+        .forEach((element) => renderPlayButton(element.parentElement));
+
+      const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(({ type, target, addedNodes }) => {
+          if (
+            type === "childList" &&
+            target.classList.contains("container") &&
+            addedNodes.length === 1
+          ) {
+            const [torrentRow] = addedNodes;
+            renderPlayButton(torrentRow.querySelector("a").parentElement);
+          }
+        });
+      });
+
+      observer.observe(torrentList[0], {
+        childList: true,
+        subtree: true,
+      });
+    }
+  }, 2000);
 });
